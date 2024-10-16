c05e51b106b111721d8d3801f506b64a78eb457a
pandablue0809 <bluep0415@gmail.com>
2024-10-16 23:48:47 +0100

    Refactor Loader component to use CSS animations for a spinning loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,5 @@
-
-
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+  return <span className="loader bottom-0 relative left-0  right-0"></span>;
+};
 
-export default Loader
+export default Loader;
