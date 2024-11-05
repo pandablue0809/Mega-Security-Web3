d85e018324cadecf66a73a3a9cc2748a1d20251b
pandablue0809 <bluep0415@gmail.com>
2024-11-05 09:34:22 +0100

    feat: Wrap App component with TransactionsProvider for context management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
+import Footer from "./components/Footer"
+import { NavBar } from "./components/NavBar";
 import Services from "./components/Services";
 import Transactions from "./components/Transactions";
 import Welcome from "./components/Welcome";
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { TransactionsProvider } from "../context/TransactionsContext";
+createRoot(document.getElementById("root")).render(
+  <TransactionsProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </TransactionsProvider>
+);
